72bf774c66c3e95ece1f2ee19dacd0b0e8057888
Vinicio Brejinski <viniciobrejinski@natura.net>
2025-07-07 20:58:03 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { sql } from './db/connetction.ts'
+import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.get('/health', async () => {
+  return { status: 'ok' }
+})
+
+app.register(getRoomsRoute)
+
+app.listen({ port: env.PORT })
